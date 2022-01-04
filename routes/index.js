953540832613e3a66db266ebfabbc82ddd1963b1
Marina Robleño <marinarsanisidro@gmail.com>
2022-01-04 16:44:57 +0100

    Added routing across the web page

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,24 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/about-us', (req, res) => {    
+  res.render('about-us');
+});
+
+router.get('/contact', (req, res) => {    
+  res.render('contact');
+});
+
+router.get('/room-list', (req, res) => {    
+  res.render('room-list');
+});
+
+router.get('/room-offers', (req, res) => {    
+  res.render('room-offers');
+});
+
+router.get('/single-room', (req, res) => {    
+  res.render('single-room');
+});
+
 module.exports = router;
