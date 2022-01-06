0a9be4af5a4d3994e6c6efbbee00301c95b267d7
Marina Robleño <marinarsanisidro@gmail.com>
2022-01-06 13:05:43 +0100

    Test checks properly that api is by default private

--- tests/sample.test.js
+++ tests/sample.test.js
@@ -1,5 +1,20 @@
-describe("Sample Test", () => {
-  it("should test that true === true", () => {
-    expect(true).toBe(true);
+const app = require("../app");
+const mongoose = require("mongoose");
+const supertest = require("supertest");
+
+beforeEach((done) => {
+  mongoose.connect("mongodb://localhost:27017/JestDB",
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    () => done());
+});
+
+afterEach((done) => {
+  mongoose.connection.db.dropDatabase(() => {
+    mongoose.connection.close(() => done())
   });
 });
+
+test("GET /api", async () => {
+    await supertest(app).get("/api")
+      .expect(401)
+  });
