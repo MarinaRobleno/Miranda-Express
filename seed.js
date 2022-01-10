92462a39a8a071b2bebbc9e4b11d20793a83c02b
Marina Robleño <marinarsanisidro@gmail.com>
2022-01-10 15:07:50 +0100

    Fixed json data in rooms and booking

--- seed.js
+++ seed.js
@@ -1,14 +1,16 @@
-const bookings = require('./json/booking');
-const rooms = require('./json/rooms');
-const {connection} = require('./db');
-var mysql = require('mysql')
+const bookings = require("./json/booking");
+const rooms = require("./json/rooms");
+const { connection } = require("./db");
+var mysql = require("mysql");
 
-connection.connect()
 
-connection.query('SELECT * FROM rooms', function (err, rows, fields) {
-    if (err) throw err
-  
-    console.log('The solution is: ', rows[0].solution)
-  })
-  
-  connection.end()
+connection.connect(function (err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+
+  console.log("connected as id " + connection.threadId);
+});
+
+connection.end();
