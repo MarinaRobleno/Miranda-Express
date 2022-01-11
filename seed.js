20e620282c62677f0c15a5c7783cdac9aa40de9e
Marina Robleño <marinarsanisidro@gmail.com>
2022-01-11 10:57:52 +0100

    Added data to rooms and bookings

--- seed.js
+++ seed.js
@@ -5,29 +5,6 @@ const users = require("./json/users");
 const { connection } = require("./db");
 var mysql = require("mysql2");
 
-/*connection.connect(function (err) {
-  if (err) throw err;
-  for (let room of rooms) {
-    const toInsert = {
-      //photo: room.photo,
-      roomNumber: room.roomNumber,
-      roomType: room.roomType,
-      amenities: room.amenities,
-      price: room.price,
-      offer_price: room.offer_price,
-      //status: room.status,
-    };
-    connection.query(
-      "INSERT INTO rooms SET?",
-      toInsert,
-      function (error, results, fields) {
-        if (error) throw error;
-        // Neat!
-      }
-    );
-  }
-});*/
-
 connection.connect(function (err) {
   if (err) throw err;
   /*for (let contact of contacts) {
@@ -48,7 +25,7 @@ connection.connect(function (err) {
         // Neat!
       }
     );
-  };*/
+  };
   for (let user of users) {
     const toInsert = {
       photo: user.photo,
@@ -69,6 +46,55 @@ connection.connect(function (err) {
       }
     );
   };
-
+  for (let room of rooms) {
+    const toInsert = {
+      roomNumber: room.roomNumber,
+      roomType: room.roomType,
+      amenities: room.amenities,
+      price: room.price,
+      offer_price: room.offer_price,
+      status: room.status,
+    };
+    connection.query(
+      "INSERT INTO rooms SET?",
+      toInsert,
+      function (error, results, fields) {
+        if (error) throw error;
+        // Neat!
+      }
+    );
+  }
+  for (let book of bookings) {
+    const toInsert = {
+      guest: book.guest,
+      orderDate: book.orderDate,
+      checkIn: book.checkIn,
+      checkOut: book.checkOut,
+      special: book.special,
+      status: book.status,
+    };
+    connection.query(
+      "INSERT INTO bookings SET?",
+      toInsert,
+      function (error, results, fields) {
+        if (error) throw error;
+        // Neat!
+      }
+    );
+  }*/
+  for (let room of rooms) {
+    for (let photo of room.photo) {
+      const toInsert = {
+        url: photo,
+      };
+      connection.query(
+        "INSERT INTO roomphotos SET?",
+        toInsert,
+        function (error, results, fields) {
+          if (error) throw error;
+          // Neat!
+        }
+      );
+    }
+  }
 });
-
