b6d2414fa773ba7a62ea77ad6345182f40046cc1
Marina Robleño <marinarsanisidro@gmail.com>
2022-01-11 09:32:48 +0100

    Functional tables for users and contacts. Data successfully added

--- seed.js
+++ seed.js
@@ -1,9 +1,11 @@
 const bookings = require("./json/booking");
 const rooms = require("./json/rooms");
+const contacts = require("./json/contact");
+const users = require("./json/users");
 const { connection } = require("./db");
 var mysql = require("mysql2");
 
-connection.connect(function (err) {
+/*connection.connect(function (err) {
   if (err) throw err;
   for (let room of rooms) {
     const toInsert = {
@@ -24,5 +26,49 @@ connection.connect(function (err) {
       }
     );
   }
+});*/
+
+connection.connect(function (err) {
+  if (err) throw err;
+  /*for (let contact of contacts) {
+    const toInsert = {
+      photo: contact.photo,
+      date: contact.date,
+      customer: contact.customer,
+      mail: contact.mail,
+      phone: contact.phone,
+      subject: contact.subject,
+      comment: contact.comment,
+    };
+    connection.query(
+      "INSERT INTO contact SET?",
+      toInsert,
+      function (error, results, fields) {
+        if (error) throw error;
+        // Neat!
+      }
+    );
+  };*/
+  for (let user of users) {
+    const toInsert = {
+      photo: user.photo,
+      name: user.name,
+      job: user.job,
+      mail: user.mail,
+      phone: user.phone,
+      status: user.status,
+      startDate: user.startDate,
+      endDate: user.endDate
+    };
+    connection.query(
+      "INSERT INTO users SET?",
+      toInsert,
+      function (error, results, fields) {
+        if (error) throw error;
+        // Neat!
+      }
+    );
+  };
+
 });
 
