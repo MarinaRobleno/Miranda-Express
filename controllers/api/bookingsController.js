34d878f2361ca70369f0f1178df8e3289d700c05
Marina Robleño <marinarsanisidro@gmail.com>
2022-01-18 14:09:53 +0100

    Each controller works properly

--- controllers/api/bookingsController.js
+++ controllers/api/bookingsController.js
@@ -15,54 +15,77 @@ const bookingSchema = new Schema({
   offer_price: Number,
   amenities: [String],
   status: String,
+  photo: [String]
 });
 
 const Booking = mongoose.model("Booking", bookingSchema);
 
 const bookingsController = {
   index: async (req, res, next) => {
-    let bookings = await Booking.find({});
-    return res.json(bookings);
+    try {
+      let bookings = await Booking.find({});
+      return res.json(bookings);
+    } catch (err) {
+      console.log(err);
+    }
   },
 
   store: async (req, res, next) => {
-    let room = await Room.findOne({ roomNumber: req.body.roomNumber }).exec();
-    let newBooking = new Booking({
-      guest: req.body.guest,
-      orderDate: req.body.orderDate,
-      checkIn: req.body.checkIn,
-      checkOut: req.body.checkOut,
-      special: req.body.special,
-      bookStatus: req.body.bookStatus,
-      roomNumber: req.body.roomNumber,
-      roomType: room.roomType,
-      price: room.price,
-      offer_price: room.offer_price,
-      amenities: room.amenities,
-      status: room.status,
-    });
-    await newBooking.save();
-    return res.json(newBooking);
+    try {
+      let room = await Room.findOne({ roomNumber: req.body.roomNumber }).exec();
+      let newBooking = new Booking({
+        guest: req.body.guest,
+        orderDate: req.body.orderDate,
+        checkIn: req.body.checkIn,
+        checkOut: req.body.checkOut,
+        special: req.body.special,
+        bookStatus: req.body.bookStatus,
+        roomNumber: req.body.roomNumber,
+        roomType: room.roomType,
+        price: room.price,
+        offer_price: room.offer_price,
+        amenities: room.amenities,
+        status: room.status,
+        photo: room.photo
+      });
+      await newBooking.save();
+      return res.json(newBooking);
+    } catch (err) {
+      console.log(err);
+    }
   },
   show: async (req, res, next) => {
-    let booking = await Booking.findOne({ _id: req.params.id }).exec();
-    return res.json(booking);
+    try {
+      let booking = await Booking.findOne({ _id: req.params.id }).exec();
+      return res.json(booking);
+    } catch (err) {
+      console.log(err);
+    }
   },
   update: async (req, res, next) => {
-    let booking = await Booking.findByIdAndUpdate(req.params.id, req.body);
-    let room = await Room.findOne({ roomNumber: req.body.roomNumber }).exec();
-    let updatedBooking = await Booking.findByIdAndUpdate(req.params.id, {
-      roomType: room.roomType,
-      price: room.price,
-      offer_price: room.offer_price,
-      amenities: room.amenities,
-      status: room.status,
-    });
-    return res.json(updatedBooking);
+    try {
+      let booking = await Booking.findByIdAndUpdate(req.params.id, req.body);
+      let room = await Room.findOne({ roomNumber: req.body.roomNumber }).exec();
+      let updatedBooking = await Booking.findByIdAndUpdate(req.params.id, {
+        roomType: room.roomType,
+        price: room.price,
+        offer_price: room.offer_price,
+        amenities: room.amenities,
+        status: room.status,
+        photo: room.photo
+      });
+      res.send(`Updated booking ID: ${req.params.id}`);
+    } catch (err) {
+      console.log(err);
+    }
   },
   delete: async (req, res, next) => {
-    let booking = await Booking.findOneAndDelete({ _id: req.params.id });
-    return res.json(booking);
+    try {
+      let booking = await Booking.findOneAndDelete({ _id: req.params.id });
+      res.send(`Deleted booking ID: ${req.params.id}`);
+    } catch (err) {
+      console.log(err);
+    }
   },
 };
 
--- controllers/api/roomsController.js
+++ controllers/api/roomsController.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const roomSchema = new Schema({
@@ -8,17 +8,22 @@ const roomSchema = new Schema({
   amenities: [String],
   price: Number,
   offer_price: Number,
-  status: String,  
+  status: String,
 });
 
 const Room = mongoose.model("Room", roomSchema);
 
 const roomsController = {
   index: async (req, res, next) => {
-    let rooms = await Room.find({});
-    return res.json(rooms);
+    try {
+      let rooms = await Room.find({});
+      return res.json(rooms);
+    } catch (err) {
+      console.log(err);
+    }
   },
   store: async (req, res, next) => {
+    try {
       const newRoom = new Room({
         roomNumber: req.body.roomNumber,
         roomType: req.body.roomType,
@@ -26,23 +31,38 @@ const roomsController = {
         price: req.body.price,
         offer_price: req.body.offer_price,
         status: req.body.status,
-        photo: req.body.photo
+        photo: req.body.photo,
       });
       await newRoom.save();
       return res.json(newRoom);
+    } catch (err) {
+      console.log(err);
+    }
   },
   show: async (req, res, next) => {
-    let room = await Room.findOne({_id: req.params.id}).exec();
-    return res.json(room);
+    try {
+      let room = await Room.findOne({ _id: req.params.id }).exec();
+      return res.json(room);
+    } catch (err) {
+      console.log(err);
+    }
   },
   update: async (req, res, next) => {
-    let room = await Room.findByIdAndUpdate(req.params.id, req.body);
-    return res.json(room);
+    try {
+      let room = await Room.findByIdAndUpdate(req.params.id, req.body);
+      res.send(`Updated room number ${req.body.roomNumber}. ID: ${req.params.id}`)
+    } catch (err) {
+      console.log(err);
+    }
   },
   delete: async (req, res, next) => {
-    let room = await Room.findOneAndDelete({ _id: req.params.id});
-    return res.json(room);
+    try {
+      let room = await Room.findOneAndDelete({ _id: req.params.id });
+      res.send(`Deleted room number ${req.body.roomNumber}. ID: ${req.params.id}`)
+    } catch (err) {
+      console.log(err);
+    }
   },
 };
 
-module.exports = {roomsController, Room};
+module.exports = { roomsController, Room };
