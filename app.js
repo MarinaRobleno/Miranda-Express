a052489a6d141008ebf07687c9483856613a525e
Marina Robleño <marinarsanisidro@gmail.com>
2022-01-31 12:16:44 +0100

    added dynamic get rooms

--- app.js
+++ app.js
@@ -55,4 +55,4 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-module.exports = app;
+module.exports = {app, db};
--- routes/index.js
+++ routes/index.js
@@ -1,31 +1,37 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const webController = require('../controllers/webpage/webController')
-const rooms = require('../json/rooms')
+//const rooms = require("../json/rooms");
+const { db } = require("../app");
+const { roomsController, Room } = require("../controllers/api/roomsController");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
 });
 
-router.get('/about-us', (req, res) => {    
-  res.render('about-us');
+router.get("/about-us", (req, res) => {
+  res.render("about-us");
 });
 
-router.get('/contact', (req, res) => {    
-  res.render('contact');
+router.get("/contact", (req, res) => {
+  res.render("contact");
 });
 
-router.get('/room-list', (req, res) => {    
-  res.render('room-list', {rooms: rooms});
+router.get("/room-list", async (req, res) => {
+  try {
+    let rooms = await Room.find({});
+    return res.render("room-list", {rooms: rooms});
+  } catch (err) {
+    console.log(err);
+  }  
 });
 
-router.get('/room-offers', (req, res) => {    
-  res.render('room-offers', {rooms: rooms});
+router.get("/room-offers", (req, res) => {
+  res.render("room-offers", { rooms: rooms });
 });
 
-router.get('/single-room', (req, res) => {    
-  res.render('single-room');
+router.get("/single-room", (req, res) => {
+  res.render("single-room");
 });
 
 module.exports = router;
