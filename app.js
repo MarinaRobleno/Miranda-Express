b810c5e7dd28ddc2fc238ae7dc6f534c20c1ca84
Marina Robleño <marinarsanisidro@gmail.com>
2023-04-28 14:39:34 +0200

    nodemon change

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
+require("dotenv").config();
+var createError = require("http-errors");
 var express = require("express");
 var path = require("path");
+var cookieParser = require("cookie-parser");
+var bodyParser = require("body-parser");
 var logger = require("morgan");
 const mongoose = require("mongoose");
 const passport = require("passport");
 const settings = require("./settings");
-const morgan = require("morgan");
-const cors = require("cors");
-
-require("dotenv").config();
-
-var app = express();
 
 mongoose.connect(
   "mongodb+srv://admin:admin@mirandacluster.kwsvf.mongodb.net/miranda_db?retryWrites=true&w=majority",
@@ -23,41 +21,6 @@ mongoose.set("useCreateIndex", true);
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
-
-
-// view engine setup
-
-app.set("view engine", "pug");
-
-app.use(express.json());
-app.use(morgan("dev"));
-app.use(cors());
-app.use(express.urlencoded({ extended: false }));
-
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PATCH, DELETE, OPTIONS, Content-Type, Authorization"
-  );
-  // res.header('Access-Control-Allow-Methods', 'Content-Type', 'Authorization');
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-
-  next();
-});
-
-app.use(express.static(path.join(__dirname, "public")));
-app.set("views", path.join(__dirname, "views"));
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-app.use(logger("dev"));
-
-
 require("./auth/auth");
 
 var indexRouter = require("./routes/index");
@@ -65,16 +28,54 @@ var usersRouter = require("./routes/users");
 var apiRouter = require("./routes/api");
 var loginRouter = require("./routes/login");
 
+var app = express();
+
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, DELETE, OPTIONS"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  next();
+});
+
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/api", apiRouter);
 app.use("/login", loginRouter);
 
 // catch 404 and forward to error handler
-// eslint-disable-next-line no-unused-vars
-app.use((err, req, resp, next) => {
-  resp.status(err.status || 500);
-  resp.json({ error: err });
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
 });
 
 module.exports = { app, db };
