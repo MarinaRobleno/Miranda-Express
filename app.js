76557e51a6690a7648888ac0806cb322fa202d7a
Marina Robleño <marinarsanisidro@gmail.com>
2023-04-28 15:07:33 +0200

    fixing cors

--- app.js
+++ app.js
@@ -1,10 +1,6 @@
 require("dotenv").config();
-var createError = require("http-errors");
 var express = require("express");
 var path = require("path");
-var cookieParser = require("cookie-parser");
-var bodyParser = require("body-parser");
-var logger = require("morgan");
 const mongoose = require("mongoose");
 const passport = require("passport");
 const settings = require("./settings");
