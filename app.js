d1933b2eade6cdfbc68beb0f4c43f3cd9709eb1f
Marina Robleño <marinarsanisidro@gmail.com>
2023-04-28 14:00:23 +0200

    cors

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ app.use(morgan("dev"));
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "http://localhost:3001");
   res.header(
     "Access-Control-Allow-Methods",
     "GET, POST, PATCH, DELETE, OPTIONS, Content-Type, Authorization"
