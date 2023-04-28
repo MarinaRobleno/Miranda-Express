0a2483008f7eea5a883839d90192a213ac3a32f9
Marina Robleño <marinarsanisidro@gmail.com>
2023-04-28 15:10:42 +0200

    fixing cors

--- app.js
+++ app.js
@@ -59,7 +59,7 @@ app.use((req, res, next) => {
   next();
 });
 
-// app.use("/", indexRouter);
+app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/api", apiRouter);
 app.use("/login", loginRouter);
