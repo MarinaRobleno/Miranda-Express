5fd51884a90d46effb4a845ca90bc3779a3883fc
Marina Robleño <marinarsanisidro@gmail.com>
2023-04-28 12:57:40 +0200

    reverted

--- env.js
+++
@@ -1,6 +0,0 @@
-const authSecret = "My_super_secret";
-const authToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7fSwiaWF0IjoxNjQyMDc0NTU2fQ.n_EsBMQ2Mg4IExjej0pMCtcOQ8zuTuPNfykb3sSjQ88";
-
-const serverDb = "mongodb+srv://admin:admin@mirandacluster.kwsvf.mongodb.net/miranda_db?retryWrites=true&w=majority";
-    
-module.exports={authSecret, serverDb, authToken}
