5b9d0a728c217586dae7b06e3e66e34ea665193a
Marina Robleño <marinarsanisidro@gmail.com>
2023-04-28 15:06:07 +0200

    fixing cors

--- app.js
+++ app.js
@@ -34,31 +34,31 @@ var loginRouter = require("./routes/login");
 var app = express();
 
 // view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "pug");
 
-app.use(logger("dev"));
+
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(cors());
-app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
 app.use(express.static(path.join(__dirname, "public")));
 app.use(passport.initialize());
 app.use(passport.session());
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Origin', '*');
   res.header(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PATCH, DELETE, OPTIONS"
+    'Access-Control-Allow-Methods',
+    'GET, POST, PATCH, DELETE, OPTIONS, Content-Type, Authorization'
   );
+  // res.header('Access-Control-Allow-Methods', 'Content-Type', 'Authorization');
   res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
   );
+
   next();
 });
 
