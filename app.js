d97ac69882e9084c8443c059df6b5b3e728f6e57
Marina <marina.robleno@invyo.io>
2023-05-08 14:58:21 +0200

    restored original route

--- app.js
+++ app.js
@@ -58,7 +58,7 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use("/miranda", indexRouter);
+app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/api", apiRouter);
 app.use("/login", loginRouter);
